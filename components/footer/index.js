cbf783d5fb3f594d49b11d3d4d43cf82f24b78fb
bguemos <bri-lynn@hotmail.com>
2024-10-24 16:23:04 -0700

    new buttons

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,48 +1,58 @@
 import styles from './Footer.module.css'
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 
 
 
 export default function Footer() {
+  const router = useRouter();
+
+  const handleNavigation = (hash) => {
+   
+    if (router.pathname === '/') {
+      document.querySelector(hash)?.scrollIntoView({ behavior: 'smooth' });
+    } else {
+    
+      router.push('/').then(() => {
+        document.querySelector(hash)?.scrollIntoView({ behavior: 'smooth' });
+      });
+    }
+  };
+
   return (
     <>
-
-    <div className={styles.main}>
-    
-    <div className={styles.logo}>
-    <Link href="/">
-    <Image
-     src='/logo/bg.png'
-     height={50}
-     width={50}
-     alt='logo'
-
-    />
-</Link>
-    </div>
-
-
-    <div className={styles.links}>
-    <Link href="/" className={styles.link}>
-    <h3 className={styles.page}>Home</h3>
-    </Link>
-    <Link href="#contact" className={styles.link} scroll={false}>
-            <h3 className={styles.page}>Contact</h3>
+      <div className={styles.main}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <Image src="/logo/bg.png" height={50} width={50} alt="logo" />
           </Link>
-          <Link href="#projects" className={styles.link} scroll={false}>
-            <h3 className={styles.page}>Projects</h3>
-          </Link>
+        </div>
 
-    
-
-
-  
-</div>
-</div>
-
+        <div className={styles.links}>
+        
+          <h3
+            className={styles.page}
+            onClick={() => handleNavigation('#home')}
+          >
+            Home
+          </h3>
+          <h3
+            className={styles.page}
+            onClick={() => handleNavigation('#contact')}
+          >
+            Contact
+          </h3>
+          <h3
+            className={styles.page}
+            onClick={() => handleNavigation('#projects')}
+          >
+            Projects
+          </h3>
+        </div>
+      </div>
+     
     </>
-   
-)
+  );
 }
--- components/header/index.js
+++ components/header/index.js
@@ -1,48 +1,55 @@
+import { useRouter } from 'next/router';
 import styles from './Header.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 
-
-
 export default function Header() {
+  const router = useRouter();
 
- return (
-            <>
+  const handleNavigation = (hash) => {
+   
+    if (router.pathname === '/') {
+      document.querySelector(hash)?.scrollIntoView({ behavior: 'smooth' });
+    } else {
     
-            <div className={styles.main}>
-            
-            <div className={styles.logo}>
-            <Link href="/">
-            <Image
-             src='/logo/bg.png'
-             height={50}
-             width={50}
-             alt='logo'
-    
-            />
-        </Link>
-            </div>
-    
-    
-            <div className={styles.links}>
-            <Link href="/" className={styles.link}>
-            <h3 className={styles.page}>Home</h3>
-            </Link>
-            <Link href="#contact" className={styles.link} scroll={false}>
-            <h3 className={styles.page}>Contact</h3>
-          </Link>
-          <Link href="#projects" className={styles.link} scroll={false}>
-            <h3 className={styles.page}>Projects</h3>
-          </Link>
-    
-            
-    
-    
-          
-    </div>
-    </div>
-    <hr></hr>
-            </>
-           
-        )
+      router.push('/').then(() => {
+        document.querySelector(hash)?.scrollIntoView({ behavior: 'smooth' });
+      });
     }
+  };
+
+  return (
+    <>
+      <div className={styles.main}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <Image src="/logo/bg.png" height={50} width={50} alt="logo" />
+          </Link>
+        </div>
+
+        <div className={styles.links}>
+        
+          <h3
+            className={styles.page}
+            onClick={() => handleNavigation('#home')}
+          >
+            Home
+          </h3>
+          <h3
+            className={styles.page}
+            onClick={() => handleNavigation('#contact')}
+          >
+            Contact
+          </h3>
+          <h3
+            className={styles.page}
+            onClick={() => handleNavigation('#projects')}
+          >
+            Projects
+          </h3>
+        </div>
+      </div>
+      <hr />
+    </>
+  );
+}
